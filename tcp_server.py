4b0598b1600e424b6f2327e5e3c79c796345e0ee
SinLapis <stradust0001@gmail.com>
2019-04-09 10:27:22 +0800

    增加tcp server

---
+++ tcp_server.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', 10201))
+    s.listen(1)
+    while True:
+        client_sock, client_addr = s.accept()
+        recv_json = ''
+        while True:
+            buf = client_sock.recv(1024)
+            if buf:
+                recv_json += buf
+            else:
+                break
+        client_sock.close()
+        print('receive json: ' + recv_json)
+
+
+if __name__ == '__main__':
+    server()
